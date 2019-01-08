0da797b4e266d610d452891d98d7d7aa6631ef85
BIT <BIT@DESKTOP-4M0FKCM>
2019-01-08 15:50:28 +0900

    NP 2Day 과제 completed

--- src/echo/EchoServer.java
+++ src/echo/EchoServer.java
@@ -1,10 +1,83 @@
 package echo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+
 public class EchoServer {
-
+	private static final int PORT = 6000;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ServerSocket serverSocket = null;
+		
+		try {
+			// 1. 서버소켓 생성
+			serverSocket = new ServerSocket();
+			
+			//2. Binding
+			String localhostAddress = InetAddress.getLocalHost().getHostAddress();
+			serverSocket.bind(new InetSocketAddress(localhostAddress, PORT));
+			log("binding " + localhostAddress + ":" + PORT);
 
+			//3. accept
+			Socket socket = serverSocket.accept();
+				
+			InetSocketAddress inetRemoteSocketAddress = (InetSocketAddress)socket.getRemoteSocketAddress();
+			log("connected by client[" + inetRemoteSocketAddress.getAddress().getHostAddress() + ":" + inetRemoteSocketAddress.getPort() + "]");
+			
+			try {
+				//4. IOStream 생성(받아오기)
+				BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+				PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"), true);
+				
+				while(true) {
+					//5. 데이터 읽기(수신)
+					String data = br.readLine();
+					if(data == null) {
+						log("closed by client");
+						break;
+					}
+					log("received:" + data);
+					
+					//6. 데이터 쓰기(전송)
+					pw.println(data);
+				}
+			} catch(SocketException e) {
+				log("abnormal closed by client");
+			} catch (IOException e) {
+				log("error:" + e);
+			} finally {
+				try {
+					//7. 자원정리(소켓 닫기)
+					if(socket != null && socket.isClosed() == false) {
+						socket.close();
+					}
+				} catch(IOException e) {
+					log("error:" + e);
+				}
+			}
+			
+		} catch (IOException e) {
+			log("error:" + e);
+		} finally {
+			try {
+				if( serverSocket != null && serverSocket.isClosed() == false) {
+					serverSocket.close();
+				}
+			} catch (IOException e) {
+				log("error:" + e);
+			}
+		}
 	}
-
+	
+	private static void log(String log) {
+		System.out.println("[server] " + log);
+	}
 }
--- src/util/NSLookup.java
+++ src/util/NSLookup.java
@@ -2,17 +2,35 @@ package util;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class NSLookup {
 
 	public static void main(String[] args) {
-		String line = "www.naver.com";
+		Scanner scanner = null;
+		
 		try {
-			InetAddress[] inetAddresses = 
-					InetAddress.getAllByName(line);
+			scanner = new Scanner(System.in);
+			
+			while(true) {
+				System.out.print(">>");
+				String line = scanner.nextLine();
+				
+				if("quit".equals(line)) {
+					break;
+				}
+				
+				InetAddress[] inetAddresses = InetAddress.getAllByName(line);
+				for(InetAddress inetAddress : inetAddresses) {
+					System.out.println(line + ":" + inetAddress.getHostAddress());;
+				}
+			}
 		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} finally {
+			if(scanner != null) {
+				scanner.close();
+			}
 		}
 		
 	}
